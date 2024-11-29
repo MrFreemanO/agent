87d81b5332f534f94e86b946b9c567b9ecbc281f
quentinzhang <tagriver@gmail.com>
2024-11-29 12:24:36 +0800

    save

--- src/App.tsx
+++ src/App.tsx
@@ -100,11 +100,15 @@ function App() {
     <div className="container">
       <div className='container_content'>
         <div className='title_container'>
-          <div>ConsoleY</div>
-          <div className='title_container_right' onClick={() => setIsSide(!isSide)}>
-            <svg className="icon_arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M5 7h14M5 12h14M5 17h14" />
-            </svg>
+          <div className="header-content" data-tauri-drag-region >
+            <div className="title">ConsoleY</div>
+          </div>
+          <div className='title_container_right none-drag-region'>
+            <div className="control-button" onClick={() => setIsSide(!isSide)}>
+              <svg className="icon_arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M5 7h14M5 12h14M5 17h14" />
+              </svg>
+            </div>
           </div>
         </div>
         <div className="vnc-container">
